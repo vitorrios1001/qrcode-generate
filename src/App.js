7b0286c15ae2598ffe41600c2cf75d67c1d029f5
Vitor Rios <vitor.neto@br.experian.com>
2021-07-16 14:09:32 -0300

    feat: adjusting home page to GA event

--- src/App.js
+++ src/App.js
@@ -1,84 +1,115 @@
 import React, { useState } from 'react';
 import QRCode from 'qrcode.react';
+import ReactGa from 'react-ga'
 
-import './style.css'
+import { useGAEvents } from './hooks/useGAEvents'
+
+import './style.css';
+
+const network = [
+  {
+    href: 'https://github.com/vitorrios1001',
+    icon: 'fa fa-github',
+    name: 'github'
+  },
+  {
+    href: 'https://www.instagram.com/vitorluizrios/',
+    icon: 'fa fa-instagram',
+    name: 'instagram'
+  },
+  {
+    href: 'https://www.linkedin.com/in/vitorluizrios',
+    icon: 'fa fa-linkedin',
+    name: 'linkedin'
+  },
+  {
+    href: 'https://www.facebook.com/vitorluizrios',
+    icon: 'fa fa-facebook-f',
+    name: 'facebook'
+  },
+];
+
+ReactGa.initialize(process.env.REACT_APP_GA_TRACKING_CODE)
 
 const App = () => {
+  const route = window.location.pathname + window.location.search
 
-  const [qrcode, setQrcode] = useState('https://br.linkedin.com/in/vitorluizrios')
+  console.log('route::', route)
 
-  const [field, setField] = useState('')
+  const GAEvents = useGAEvents('External Links')
 
-  const handleQrCode = e => {
-    setField(e.target.value)
-  }
+  React.useEffect(() => {
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
+
+  const [qrcode, setQrcode] = useState(
+    'https://br.linkedin.com/in/vitorluizrios'
+  );
+
+  const [field, setField] = useState('');
+
+  const handleQrCode = (e) => {
+    setField(e.target.value);
+  };
 
   const generateQrCode = (e) => {
     e.preventDefault();
     setQrcode(field);
-  }
+  };
 
-  const clear = () => setField('')
+  const clear = () => setField('');
 
   return (
-
-    <div className="container" >
+    <div className='container'>
       <header>
-        <h2><i className="fa fa-qrcode" /> QRCode Generator</h2>
+        <h2>
+          <i className='fa fa-qrcode' /> QRCode Generator
+        </h2>
       </header>
 
-
-      <div className="container-qr-code">
-        <div className="qr-code">
+      <div className='container-qr-code'>
+        <div className='qr-code'>
           <QRCode size={250} value={qrcode} />
         </div>
-        <span className="result">Generated: {qrcode}</span>
+        <span className='result'>Generated: {qrcode}</span>
       </div>
 
       <form onSubmit={generateQrCode}>
-
         <input
           placeholder='Value'
           name='field'
           value={field}
           onChange={handleQrCode}
         />
-        <div className="buttons">
-          <button
-            className='button-clear'
-            type='button'
-            onClick={clear}
-          >
-            <i className="fa fa-trash" />
+        <div className='buttons'>
+          <button className='button-clear' type='button' onClick={clear}>
+            <i className='fa fa-trash' />
           </button>
-          <button
-            type='submit'
-            className='button-generate'
-          ><i className="fa fa-magic" /> Generate</button>
+          <button type='submit' className='button-generate'>
+            <i className='fa fa-magic' /> Generate
+          </button>
         </div>
-
       </form>
 
       <footer>
         <h5>Produced by: Vitor Rios</h5>
         <div className='network'>
-          <a href='https://github.com/vitorrios1001' target="_blank" rel="noopener noreferrer">
-            <i className="fa fa-github"></i>
-          </a>
-          <a href='https://www.instagram.com/vitorluizrios/' target="_blank" rel="noopener noreferrer">
-            <i className="fa fa-instagram" />
-          </a>
-          <a href='https://www.linkedin.com/in/vitorluizrios' target="_blank" rel="noopener noreferrer">
-            <i className="fa fa-linkedin" />
-          </a>
-          <a href='https://www.facebook.com/vitorluizrios' target="_blank" rel="noopener noreferrer">
-            <i className="fa fa-facebook-f" />
-          </a>
+          {network.map((item) => (
+            <a
+              key={item.href}
+              href={item.href}
+              target='_blank'
+              rel='noopener noreferrer'
+              onClick={() => GAEvents(item.name, item.href)}
+            >
+              <i className={item.icon} />
+            </a>
+          ))}
         </div>
         {/* <a href='https://github.com/vitorrios1001' target="_blank">github/vitorrios1001</a> */}
       </footer>
     </div>
   );
-}
+};
 
 export default App;
